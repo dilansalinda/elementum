2457d8fb8c37a4eaf4593db8c646fafb1f5e03a5
dilansalinda <contact.dilansalinda@gmail.com>
2025-10-06 06:15:49 +0530

    enabled PWA

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'elementum-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/icons/icon-192x192.png',
+  '/assets/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
